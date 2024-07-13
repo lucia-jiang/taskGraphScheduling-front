0be4164eac2846a23fef3e49d65ba819b5033bf3
lucia-jiang <luciajiangguo@gmail.com>
2024-07-13 14:53:20 +0100

    Changes in texts

--- src/components/games/TimeChallenge.js
+++ src/components/games/TimeChallenge.js
@@ -10,14 +10,13 @@ const TimeChallenge = ({}) => {
     return (<div className={"rounded-square"}>
         <h1>Time challenge</h1>
         <p1>
-            Game Time Challenge is a game where you aim to efficiently schedule tasks across multiple processors. It
+            Time Challenge is a game where you aim to efficiently schedule tasks across multiple processors. It
             presents you with a unique set of interdependent tasks represented as a directed acyclic graph (DAG). Your
             goal is to minimise the total completion time by optimising the order of task execution and strategically
             assigning tasks to processors. Beware of communication costs between processors and make sure to keep idle
             time to a minimum. It's a race against the clock as you strive to assign processors to all nodes before time
             runs out. Aim to complete the scheduling <strong>under the given time limit</strong> to prove your
-            scheduling prowess and
-            achieve the best time possible!
+            scheduling prowess and achieve the best time possible!
         </p1>
         <Button onClick={() => navigate('/game/time-challenge')} className={"mt-2"}>Challenge accepted!</Button>
     </div>);
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,34 +15,36 @@ const Home = () => {
                     <h2>What is Task Graph Scheduling?</h2>
                     <p>
                         Task graph scheduling problems involve efficiently <strong>assigning interdependent tasks to
-                        multiple
-                        processors</strong>. These tasks, along with their dependencies, are represented using <strong>directed
-                        acyclic
-                        graphs (DAGs)</strong>. In these graphs, nodes represent tasks, and edges indicate dependencies
-                        between
-                        tasks. Each node has a weight that signifies the time required to complete the task, while each
-                        edge has a weight representing the communication cost for transferring data between tasks
-                        assigned to different processors. If the predecessor task and the current task are assigned to
-                        the same processor, no communication cost is incurred. Additionally, a key constraint in task
-                        scheduling is that each processor can only handle one task at a time.
+                        multiple machines.</strong> These tasks, along with their dependencies, are represented using
+                        directed acyclic graphs (DAGs). In these graphs, nodes represent tasks, and edges indicate
+                        dependencies between tasks. Each node has a <strong>weight</strong> that signifies the time
+                        required to complete the task, while each edge has a <strong>cost</strong> representing the
+                        communication cost for transferring data or materials between tasks assigned to different
+                        machines. <strong>If the predecessor task and the current task are assigned to the same machine,
+                        no communication cost is incurred</strong>. Additionally, a key constraint in task
+                        scheduling is that <strong>each machine can only handle one task at a time</strong>, without any
+                        OS-style time-slicing.
                     </p>
                     <p>
                         The primary goal of task graph scheduling algorithms is to <strong>minimise the total completion
-                        time of
-                        all tasks.</strong> This involves not only optimising the order in which tasks are executed but
-                        also
-                        strategically assigning tasks to processors to reduce communication overhead. Effective
-                        scheduling ensures a balanced workload distribution among processors and minimises idle time,
-                        leading to improved performance in computational systems. These algorithms are crucial in
-                        parallel computing environments, where tasks are often interdependent and must be executed in a
-                        specific sequence.
-                    </p>
+                        time of all tasks</strong>. This involves not only optimising the order in which tasks are
+                        executed but also strategically assigning tasks to machines to reduce communication overhead.
+                        Effective scheduling ensures a balanced workload distribution among machines and minimises idle
+                        time, leading to improved performance in various systems, including manufacturing and production
+                        environments.</p>
                     <p>
                         It's important to note that while these algorithms provide good enough solutions, the underlying
-                        problem is <strong>NP-complete.</strong> This means that there is no known polynomial-time solution that
-                        guarantees finding the optimal solution in all cases. Therefore, task graph scheduling
-                        algorithms focus on heuristic and approximation methods to find near-optimal solutions within a
-                        reasonable time frame.
+                        problem is <strong>NP-complete.</strong> This means that there is no known polynomial-time
+                        solution that guarantees finding the optimal solution in all cases. Therefore, task graph
+                        scheduling algorithms focus on heuristic and approximation methods to find near-optimal
+                        solutions within a reasonable time frame.
+                    </p>
+                    <p>
+                        While task graph scheduling is a fundamental problem in computer science, <strong>it extends far
+                        beyond that field</strong>. In many real-world applications, the "machines" could be various
+                        types of equipment in a factory, and the "tasks" may not be software programs but physical
+                        operations. This versatility underscores the importance of these algorithms in optimizing
+                        workflows and resources in diverse industries.
                     </p>
                     <div className="row mb-4">
                         <div className="col-6 mb-3">
@@ -52,7 +54,8 @@ const Home = () => {
                             <Link to="/users-solve-problem" className="btn btn-primary w-100">Users Solve Problem</Link>
                         </div>
                         <div className="col-6 mb-3">
-                            <Link to="/create-customised-graph" className="btn btn-primary w-100">Create Customised Graph</Link>
+                            <Link to="/create-customised-graph" className="btn btn-primary w-100">Create Customised
+                                Graph</Link>
                         </div>
                         <div className="col-6 mb-3">
                             <Link to="/games" className="btn btn-primary w-100">Games</Link>
