63bc02e14a1a644a5ced75e865927d05369253d3
lucia-jiang <luciajiangguo@gmail.com>
2024-07-14 11:09:06 +0100

    Styling button

--- src/components/algorithm/AlgorithmExplanation.js
+++ src/components/algorithm/AlgorithmExplanation.js
@@ -1,30 +1,31 @@
-import React from 'react'
-import PropTypes from "prop-types"
-import "../Components.css"
-import {Button} from "react-bootstrap";
+import React from 'react';
+import PropTypes from 'prop-types';
 import {useNavigate} from 'react-router-dom';
+import "../Components.css";
 
 const AlgorithmExplanation = ({algorithmTitle, algorithmName, desc}) => {
     const navigate = useNavigate();
 
     return (
-        <div className={"rounded-square"}>
-            <h1> {algorithmTitle} </h1>
-            <p1 className={"mt-2"}>{desc}</p1>
-            <div className={"row mt-4"}>
-                <div className={"col-md-6 mb-2"}>
-                    <Button onClick={() => navigate(`/algorithms/${algorithmName}`)}> See an example</Button>
-                </div>
-                <div className={"col-md-6"}>
-                    <Button onClick={() => navigate(`/create-customised-graph/${algorithmName}`)}> Solve customised
-                        graph</Button>
-                </div>
+        <div className="rounded-square">
+            <h1>{algorithmTitle}</h1>
+            <p className="mt-2">{desc}</p>
+            <div className="button-container">
+                <button className="custom-button" onClick={() => navigate(`/algorithms/${algorithmName}`)}>
+                    See an example
+                </button>
+                <button className="custom-button" onClick={() => navigate(`/create-customised-graph/${algorithmName}`)}>
+                    Solve customised graph
+                </button>
             </div>
-        </div>)
-}
+        </div>
+    );
+};
 
 AlgorithmExplanation.propTypes = {
-    algorithmTitle: PropTypes.string.isRequired, desc: PropTypes.string.isRequired,
-}
+    algorithmTitle: PropTypes.string.isRequired,
+    algorithmName: PropTypes.string.isRequired,
+    desc: PropTypes.string.isRequired,
+};
 
-export default AlgorithmExplanation
+export default AlgorithmExplanation;
